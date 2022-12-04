26671b91ce8ff50f215c32053adc624839c8d7a7
Nish-name <nisthasoni95@gmail.com>
2022-12-04 18:42:13 +0530

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var icon = document.getElementById("icon");
-icon.onclick = function() {
-    document.body.classList.contains("dark-theme");
-    if (document.body.classList.contains("dark-theme")){
-        icon.src ="images/sun.png";
-    }
-    else{
-        icon.src="images/moon.png";
-    }
-    }
+function myFunction() {
+    var element=document.body;element.classList.toggle("dark-mode")
+}
+
+async function loadCoupon() {
+    document.getElementById('coupon').style.display = 'block';
+    document.getElementById('coupon').style.marginTop = '0px';
+    document.body.style.overflow = 'hidden';
+    document.getElementById('main').style.opacity = '0.6';
+    checkLocation()
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.display = 'none';
+    document.getElementById('main').style.opacity = '1';
+    document.body.style.overflow = 'scroll';
+}
