dd9917a028d5d8e43e2f069fa43d61cb7eb8ae54
Nish-name <nisthasoni95@gmail.com>
2022-12-11 14:37:44 +0530

    third commit

--- index.js
+++ index.js
@@ -12,3 +12,38 @@ function closecoupen() {
     document.getElementById('main').style.opacity = '1' ;
 }
 
+//geo location//
+
+let x = document.getElementById('out');
+let y = document.getElementById('weather');
+
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }else{
+        x.innerText = "Geo Not Supported"
+    }
+}
+
+function showPosition(data){
+    console.log(data);
+    let lat = data.coords.latitude;
+    let long = data.coords.longitude;
+    let out = `Latitude is ${lat} & longitude is ${long}`
+    x.innerText = out
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    ///api calling
+    fetch(url,{method: 'GET'})
+    //return promise
+    .then((res) => res.json())
+    //return data
+    .then((data) => {
+        console.log(data)
+        let city = data.city.name
+        let temp = data.list[0].temp.day
+        y.innerText = `Your city is ${city} and temp is ${temp} °C`
+    })
+}
+
+
+
